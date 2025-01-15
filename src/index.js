50fc3b964b3e9631d1ae641939e6b256e6050f8d
sehoon <kyung6805@naver.com>
2025-01-15 16:37:10 +0900

    기본경로 추가가

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import MarketingPage from './marketing-page/MarketingPage'; 
 
@@ -11,7 +10,7 @@ root.render(
  <BrowserRouter>
     <Routes>
       <Route path="/" exact element={<MarketingPage />} />
-  
+      <Route path="*" element={<Navigate to="/" replace />} /> 
     </Routes>
   </BrowserRouter>
 );
