f4ece25cdabea83f2c3147d44726008d2397d637
sehoon <kyung6805@naver.com>
2025-01-16 17:11:27 +0900

    로그인,소개,작업화면면

--- src/aboutus-page/aboutus.js
+++ src/aboutus-page/aboutus.js
@@ -2,16 +2,16 @@ import React, {useRef} from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppTheme from '../shared-theme/AppTheme';
 import AppAppBar from './components/AppAppBar';
-import FAQ from './components/FAQ';
 import Footer from './components/Footer';
+import Highlights from './components/Highlights'
 
 export default function FAQPage(props) {
   return (
     <AppTheme {...props}>
       <CssBaseline enableColorScheme />
       <AppAppBar />
+      <Highlights></Highlights>
       <div>
-        <FAQ />
         <Footer />
       </div>
     </AppTheme>
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import SignUp from './sign-up/SignUp';
 import Agree from './sign-up/Agree';
 import FAQPage from './FAQ-page/FAQPage';
 import QuestionPage from './question-page/QuestionPage';
+import AboutUs from './aboutus-page/aboutus';
+import Edit from './edit-page/EditPage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,6 +23,10 @@ root.render(
       <Route path="*" element={<Navigate to="/" replace />} /> 
       <Route path="/agree" element={<Agree />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/aboutus" element={<AboutUs />} />
+      <Route path="/edit" element={<Edit />} />
+
+      
     </Routes>
   </BrowserRouter>
 );
