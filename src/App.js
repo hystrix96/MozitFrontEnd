755bd4bd22cc5774f701063b34da1119176967d6
yeonju <919jung@gmail.com>
2025-02-03 16:07:51 +0900

    scroll to top

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import PublicRoutes from './routes/PublicRoute';
 import AdminRoutes from './routes/AdminRoute';
+import ScrollToTop from './ScrollToTop';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/admin/*" element={<AdminRoutes />} />
-        <Route path="/*" element={<PublicRoutes />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/admin/*" element={<AdminRoutes />} />
+          <Route path="/*" element={<PublicRoutes />} />
+        </Routes>
+      </ScrollToTop>
     </Router>
   );
 };
