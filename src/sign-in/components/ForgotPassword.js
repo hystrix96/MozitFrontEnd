a8bc03a0314de88dca2a0b71091b21b7653ca7d9
sehoon <kyung6805@naver.com>
2025-02-04 17:04:19 +0900

    비밀번호 찾기

--- src/sign-in/components/ForgotPassword.js
+++ src/sign-in/components/ForgotPassword.js
@@ -172,11 +172,13 @@ function ForgotPassword({ open, handleClose }) {
       {
         withCredentials: true, 
       });
-
+      console.log(response);
       if (response.status === 200) {
         setTimer(0);
         alert('이메일 인증이 완료되었습니다.');
-        settemporaryToken(response.headers['temporary-token'])
+        const temporaryToken = response.headers.get('Temporary-Token');
+        settemporaryToken(temporaryToken);
+        console.log("응답확인:",temporaryToken);
         handleNext();
       } else {
         settemporaryToken('')
@@ -192,12 +194,13 @@ function ForgotPassword({ open, handleClose }) {
     if(passwordVerified && confirmpasswordVerified){
       try {
         // 비밀번호 리셋을 위한 백엔드 API 호출
+        console.log("보내는 토큰: ", temporaryToken);
         const response = await axios.post('http://localhost:8080/users/reset-password', {
           "new-pwd": password,
         },
         {
           headers: {
-            Authorization: `${temporaryToken}`,
+            Authorization: `Bearer ${temporaryToken}`,
           },
         });
 
