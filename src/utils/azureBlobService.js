186445af3da604ffab7fde310e788651171746d8
seoyunho <seoyunho@gmail.com>
2025-02-10 15:10:35 +0900

    azure 환경변수에 sas_token 적용 (CI=false 있는 채로 npm run build 가능)

--- src/utils/azureBlobService.js
+++ src/utils/azureBlobService.js
@@ -1,8 +1,15 @@
 import { BlobServiceClient, newPipeline, AnonymousCredential } from '@azure/storage-blob';
 
-const AZURE_STORAGE_ACCOUNT_NAME = 'mozitstorage';
-const AZURE_CONTAINER_NAME = 'mozit-container';
-const SAS_TOKEN = 'sp=racwd&st=2025-02-07T14:49:12Z&se=2025-02-19T22:49:12Z&spr=https&sv=2022-11-02&sr=c&sig=lmIYpzdacUYflxTuybd9eUpCK9WRHzDdIAE%2F%2BpI3x%2FQ%3D';
+// const AZURE_STORAGE_ACCOUNT_NAME = 'mozitstorage';
+// const AZURE_CONTAINER_NAME = 'mozit-container';
+// const SAS_TOKEN = 'sp=racwd&st=2025-02-07T14:49:12Z&se=2025-02-19T22:49:12Z&spr=https&sv=2022-11-02&sr=c&sig=lmIYpzdacUYflxTuybd9eUpCK9WRHzDdIAE%2F%2BpI3x%2FQ%3D';
+const AZURE_STORAGE_ACCOUNT_NAME = process.env.REACT_APP_AZURE_STORAGE_ACCOUNT_NAME;
+const AZURE_CONTAINER_NAME = process.env.REACT_APP_AZURE_CONTAINER_NAME;
+const SAS_TOKEN = process.env.REACT_APP_SAS_TOKEN;
+
+if (!SAS_TOKEN) {
+  throw new Error("SAS_TOKEN is not set in the environment variables");
+}
 
 // Create a pipeline using AnonymousCredential (or SAS token)
 const blobServiceClient = new BlobServiceClient(
